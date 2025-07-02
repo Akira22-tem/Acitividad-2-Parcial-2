6c5811772461eb1eaa2ef4ed4759e0aa1777433a
Kevin Yugla <kiyugla@espe.edu.ec>
2025-07-01 22:45:10 -0500

    completado los archivos a cachear

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,16 @@
 const CACHE_NAME = 'facturacion-cache_v1';
-const archivosParaCachear = ['./', './index.html'];
+const archivosParaCachear = [
+  './',
+  './index.html',
+  './clientes.html',
+  './productos.html',
+  './facturacion.html',
+  './facturas.html',
+  './public/js/app.js',
+  './public/lib/bootstrap-5.3.6-dist/css/bootstrap.min.css',
+  './public/lib/bootstrap-5.3.6-dist/js/bootstrap.bundle.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.11.1/font/bootstrap-icons.min.css',
+];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
