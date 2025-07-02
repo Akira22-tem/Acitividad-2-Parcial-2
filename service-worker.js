4fa219741094e881e40af65faea0fd6508e6b726
Kevin Yugla <kiyugla@espe.edu.ec>
2025-07-01 22:49:40 -0500

    arreglo completo del self.addEevente de instalar arreglado

--- service-worker.js
+++ service-worker.js
@@ -11,11 +11,17 @@ const archivosParaCachear = [
   './public/lib/bootstrap-5.3.6-dist/js/bootstrap.bundle.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.11.1/font/bootstrap-icons.min.css',
 ];
-
 self.addEventListener('install', (event) => {
+  console.log('[Service Worker] Instalando...');
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(archivosParaCachear);
-    })
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        console.log('[Service Worker] Cacheando archivos esenciales');
+        return cache.addAll(archivosParaCachear);
+      })
+      .catch((error) => {
+        console.error('[Service Worker] Error al cachear:', error);
+      })
   );
 });
